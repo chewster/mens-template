9a57c1a5e9d405aff556fd25da92b2c21c95de88
chewster <cmchiu.chiu@gmail.com>
2018-06-10 04:15:21 -0400

    mimic the mocks

---
+++ README.md
@@ -0,0 +1,30 @@
+# Men template
+
+made for demo
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+### Prerequisites
+
+have http-server
+```
+http-server
+```
+
+### Installing
+
+If you don't have http-server
+
+```
+npm install http-server
+
+```
+then you would be able to run this in your terminal 
+```angular2html
+http-server
+
+```
+until finished
+```
--- src/script.js
+++ src/script.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
-    $('#add-module').click(function() {
-
+    $('.module-type').click(function(){
+        $('.module-type').removeClass('active');
+        moduletype=$(this).attr('id');
+        $(this).toggleClass('active');
+    });
+    $('.continue').click(function(){
+        $('.module-type').removeClass('active');
+        //$('.modal.fade').removeClass('in');
+        $.get("../templates/"+moduletype+".html", function(data){
+            $('.container.info').append(data);
+        });
     });
 });
